b45937281f6b31edf693551bbba1ffcdfd21d0c1
daniele cammarata <brusaurio@gmail.com>
2013-06-21 15:28:08 +0200

    Creazione lorenzocammarata MVC

---
+++ lorenzocammarata/Controllers/LorenzoController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+namespace lorenzocammarata.Controllers
+{
+    public class LorenzoController : Controller
+    {
+        //
+        // GET: /Lorenzo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Io()
+        {
+            return View();
+        }
+
+        public ActionResult Famiglia()
+        {
+            return View();
+        }
+
+        public ActionResult Amici()
+        {
+            return View();
+        }
+    }
+}
